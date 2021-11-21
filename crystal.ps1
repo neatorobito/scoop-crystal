8e5e0059a9d7afc0c91638c63c6f4de602925e25
Mark Raymond Jr <markrjr@live.com>
2021-11-20 21:25:38 -0800

    Add powershell shim to load dev env

---
+++ crystal.ps1
@@ -0,0 +1,17 @@
+$cwd = Get-Location
+$installationPath = vswhere.exe -prerelease -latest -property installationPath
+if ($installationPath -and (test-path "$installationPath\Common7\Tools\vsdevcmd.bat")) {
+  
+  if(!$env:READY_FOR_CRYSTAL) {
+    & "${env:COMSPEC}" /s /c "`"$installationPath\Common7\Tools\vsdevcmd.bat`" -no_logo -arch=amd64 && set" | foreach-object {
+      $name, $value = $_ -split '=', 2
+      set-content env:\"$name" $value
+    }
+    $env:READY_FOR_CRYSTAL = $true
+  } 
+
+  & $cwd\crystal.exe $args
+}
+else {
+    "Can't find the Visual Studio Build Tools, please verify your installation."
+}
