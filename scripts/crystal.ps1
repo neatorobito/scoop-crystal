1058505a5e66282040d0226cd74c5f507bd7dc8a
Mark Raymond Jr <markrjr@live.com>
2021-11-24 12:50:15 -0600

    Add support for full VS installations or build tools

--- scripts/crystal.ps1
+++ scripts/crystal.ps1
@@ -1,5 +1,19 @@
- $installationPath = vswhere.exe -latest -products 'Microsoft.VisualStudio.Product.BuildTools' -property installationPath
- if ($installationPath -and (test-path "$installationPath\Common7\Tools\vsdevcmd.bat")) {
+$buildToolsPath = vswhere.exe -latest -products 'Microsoft.VisualStudio.Product.BuildTools' -property installationPath
+$fullInstallPath = vswhere.exe -version '[15.0,)' -property installationPath
+$installationPath = ""
+
+if($buildToolsPath) {
+  $compilersPath = $buildToolsPath
+}
+else if($fullInstallPath){
+  $compilersPath = $fullInstallPath
+}
+else
+{
+  "Can't find a full Visual Studio installation or a Visaul Studio Build Tools installation."
+}
+
+if ($installationPath -and (test-path "$installationPath\Common7\Tools\vsdevcmd.bat")) {
   
   if(!$env:READY_FOR_CRYSTAL) {
     & "${env:COMSPEC}" /s /c "`"$installationPath\Common7\Tools\vsdevcmd.bat`" -no_logo -arch=amd64 && set" | foreach-object {
@@ -12,5 +26,5 @@
   & _crystal.exe $args
 }
 else {
-    "Can't find the Visual Studio Build Tools, please verify your installation."
+    "Can't find MSVC to load a dev environment, please verify the following workloads and components are installed: Microsoft.VisualStudio.Workload.VCTools and Microsoft.VisualStudio.Component.VC.ATL"
 }
